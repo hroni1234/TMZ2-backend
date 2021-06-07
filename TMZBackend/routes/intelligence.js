462bcdd63f5af1983c1278e043416fd6ecf0b220
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 17:12:37 +0300

    fix(dbVairiablesPod.json): fixed db address

--- TMZBackend/routes/intelligence.js
+++ TMZBackend/routes/intelligence.js
@@ -3,25 +3,33 @@ const router = express.Router();
 const intelligenceService = require("../services/intelligenceService");
 const badRequestCode = 400;
 
-const sendFromPromise = (promise, res,errCode) =>
+const sendFromPromise = (promise, res, errCode) =>
   promise
     .then(response => res.send(response))
     .catch(err => res.status(errCode).send(err));
 
 router.get("/id/:id", (req, res) =>
-  sendFromPromise(intelligenceService.profileById(req.params.id), res,badRequestCode)
+  sendFromPromise(
+    intelligenceService.profileById(req.params.id),
+    res,
+    badRequestCode
+  )
 );
 
-router.get("/all/profiles", (req,res) =>
-  sendFromPromise(intelligenceService.allProfiles(), res,badRequestCode)
+router.get("/all/profiles", (req, res) =>
+  sendFromPromise(intelligenceService.allProfiles(), res, badRequestCode)
 );
 
-router.get("/number/suspectsRequested", (req,res) =>
-  sendFromPromise(intelligenceService.numberOfSuspectsRequested(), res,badRequestCode)
+router.get("/number/suspectsRequested", (req, res) =>
+  sendFromPromise(
+    intelligenceService.numberOfSuspectsRequested(),
+    res,
+    badRequestCode
+  )
 );
 
 router.get("/all/suspectsRequested", (req, res) =>
-  sendFromPromise(intelligenceService.suspectsRequested(), res,badRequestCode)
+  sendFromPromise(intelligenceService.suspectsRequested(), res, badRequestCode)
 );
 
 module.exports = router;
