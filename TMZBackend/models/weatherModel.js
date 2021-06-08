298f568037deafe669a88dfac02a0ef4b00cb0fb
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 17:52:08 +0300

    fix(weatherModel): fix ip

--- TMZBackend/models/weatherModel.js
+++ TMZBackend/models/weatherModel.js
@@ -1,6 +1,6 @@
 const api = require("../api");
 const securityBaseUrl =
-  "19.00.4:30000/2";
+  "19.0.0.4:30000/2";
 
   const weatherDates = async () =>
   await api.getData(`${securityBaseUrl}/weatherDates`);
