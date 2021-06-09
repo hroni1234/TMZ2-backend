cad27de463503ae77a18dc2607623c30746f9800
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 11:27:48 +0300

    logs weather

--- TMZBackend/routes/weather.js
+++ TMZBackend/routes/weather.js
@@ -4,10 +4,12 @@ let weatherData = {};
 
 router.post("", (req, res) => {
   weatherData = req.body;
+  console.log(weatherData + "post");
 });
 
 router.get("", (req, res) => {
 res.send(weatherData);
+console.log(weatherData + "get");
 });
 
 module.exports = router;
